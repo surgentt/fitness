4dc8ba182a7cce12da621906c9878b52e3a79fa7
Thomas Surgent <surgentt@gmail.com>
2013-10-04 17:13:43 -0400

    Good Update

--- application.js
+++ application.js
@@ -1,19 +1,18 @@
-
-
-
+//Load Jquery Library
 $(document).ready(function(){
 
   console.log(d3);
 
   //Declare the following variables
-  var i   = 0;   // interest
-  var m   = 0;   // months
+  var i   = 10;   // interest
+  var m   = 400;   // months
   var f   = 0;   // factor
   var den = 0;   // denominator
   var s   = "0"; // string
   var d   = 0;   // decimal place
 
   $("#howMuch").click(function() {
+    //Check if any portions of the site are blank
     if(document.calform.interest.value=="" || document.calform.months.value=="" || document.calform.payment.value=="") {
       //Display a popup 
       alert("Please fill in all of the required fields.");
@@ -27,11 +26,17 @@ $(document).ready(function(){
         m = document.calform.months.value;
       }
 
+      // Create a Graph on Click
+
+      // create a variable for data and set it equal to an empty array
       var data = [];
+      //Declare a variable index; Pass in months; Increment by one month on each loop
       for(var index = 0; index <= m; index++) {
-        data.push({ x: index, y: calculate(index) });
+        //Add the new data to the end of the Array
+        data.push({ x: index, y: future_value(index) });
       }
       console.log(data);
+      // Create a new Rickshaw Graph
       var graph = new Rickshaw.Graph( {
           element: document.querySelector("#chart"), 
           width: 500, 
@@ -43,11 +48,13 @@ $(document).ready(function(){
           }]
       });
 
+      //Render the graph
       graph.render();
     }
   });
 
-  function calculate(m) {
+  //calculate Future Value of an Interst Rate
+  function future_value(m) {
 
     //   ((i/100)+1)^.08333)-1
     i = Math.pow(((document.calform.interest.value/100)+1),.0833333)-1;
@@ -64,9 +71,9 @@ $(document).ready(function(){
     d = String(f).indexOf(".");
     // Create a string "s" add a decimal point in
     s = String(f).substring(0,(d+3));
+    document.calform.total.value = "$" + s;
     var value = parseFloat(s);
     return value;
   }
 });
-
 
