549d4b0527aa360750d09f8d18d9be6300974197
Thomas Surgent <surgentt@gmail.com>
2013-11-19 18:16:42 -0500

    great job

--- graph.js
+++ graph.js
@@ -8,24 +8,36 @@ $(document).ready(function(){
   });
 
 	//Run Code on Click of id="how Much"
-	$("#howMuch").click(function(ev) {
-		var numOfPeriods = 0; 
-    console.log("Gloabl Starting N =" + numOfPeriods);
+  $("#howMuch").click(function () {
+    var numOfPeriods = 0;
+    //Validates for empty code only. 
+    //Loop through all the required fields. If any are blank jump out
+    //http://stackoverflow.com/questions/20082915/jquery-required-fields-loop-based-on-css-class
+    $('.required').each(function() {
+      if($(this).val().length == 0) {
+        $('.modal').show();
+        $('.modal_content').text('Please fill in the Required Fields');
+        //If the first field is filled in make sure to catch blank feild and react occording on the second go through
+        clearGraph();
+        return false;
+      }
+    });
 
-    //Validates for empty code only.
-		if($("#pmt").val().length == 0 || $("#numOfPeriods").val().length == 0 ){
-			$('.modal').show();
-      $('.modal_content').text('Please fill in the Required Fields');
-		} else {
-      //Clear previous graph
-      clearGraph();
-      //Adjust the number of periods from years to month
-      numOfPeriods = yearToMonthAdj();
-      //Declare a variable for the final future value
-			var futureValue = createiRateGraphNoInterest(numOfPeriods);
-      //Test if were rich
-      testFVforMillionaire(futureValue);
- 		}						
+    //Clear previous graph
+    clearGraph();
+    //Adjust the number of periods from years to month
+    numOfPeriods = yearToMonthAdj();
+
+    // Change function that is run, depending on what the body of the page is named
+    if($('body.withoutInterest')[0]) {
+      var futureValue = createiRateGraphNoInterest(numOfPeriods);
+    } else if ($('body.withInterest')[0]) {
+      var futureValue = createiRateGraph(numOfPeriods);
+    } else if ($('body.bothInterest')[0]) {
+      var futureValue = createBoth(numOfPeriods);
+    }
+
+    testFVforMillionaire(futureValue);
   });
 
   function clearGraph() {
@@ -57,6 +69,17 @@ $(document).ready(function(){
     return futureValue;
   }
 
+  function runFVOrdinaryAnnuity(numOfPeriods, pmt, interest) {
+    var factor = 0;
+    var futureValue = 0;
+    var futureValueString = "0";
+    
+    //fv = pmt*[(((1+i)^n)-1/i)]
+    factor = ((Math.pow((1+interest),numOfPeriods))-1)/interest;
+    futureValue = pmt * factor;
+    
+    return futureValue;
+  }
 
   function createiRateGraphNoInterest(numOfPeriods) {
     //Declare an empty array to pass data into
@@ -70,36 +93,131 @@ $(document).ready(function(){
     }
     console.log(data);
     // Create a new Rickshaw Graph
-      var graph = new Rickshaw.Graph( {
-        element: document.querySelector("#chart"), 
-        //Automatically Scale to window size
-        //width: 500, 
-        //height: 200, 
-        series: [{
-          color: 'steelblue',
-          name:  'total savings',
-          data: data
-        }]
-      });
+    var graph = new Rickshaw.Graph( {
+      element: document.querySelector("#chart"), 
+      //Automatically Scale to window size
+      series: [{
+        color: 'steelblue',
+        name:  'total savings',
+        data: data
+      }]
+    });
 
-      var x_axis = new Rickshaw.Graph.Axis.X( { 
-        graph: graph,
-        element: document.getElementById('x_axis')
-      });
+    var x_axis = new Rickshaw.Graph.Axis.X( { 
+      graph: graph,
+      element: document.getElementById('x_axis')
+    });
 
-      var y_axis = new Rickshaw.Graph.Axis.Y( {
-        graph: graph,
-        orientation: 'left',
-        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-        element: document.getElementById('y_axis'),
-      });
+    var y_axis = new Rickshaw.Graph.Axis.Y( {
+      graph: graph,
+      orientation: 'left',
+      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+      element: document.getElementById('y_axis'),
+    });
 
-      graph.render();
+    graph.render();
 
-      futureValueString = "$" + futureValue.formatMoney(2);
-      $('#total').val(futureValueString);
+    futureValueString = "$" + futureValue.formatMoney(2);
+    $('#total').val(futureValueString);
 
-      return futureValue;
+    return futureValue;
+  }
+
+  function createiRateGraph(numOfPeriods) {
+    //Declare an empty array to pass data into
+    var data = [];
+    var interest = $('#interest').val()/100/12;
+    var pmt = $('#pmt').val()
+
+    //Declare a variable index; Pass in months; Increment by one month on each loop
+    for(var index = 0; index <= numOfPeriods; index++) {
+      //Instead of passing in numOfPeriods, index is passed into runFvNoInterest)
+      futureValue = runFVOrdinaryAnnuity(index, pmt, interest);
+      data.push({ x: index, y: futureValue });
+    }
+    console.log(data);
+    // Create a new Rickshaw Graph
+    var graph = new Rickshaw.Graph( {
+      element: document.querySelector("#chart"), 
+      //Automatically Scale to Window Size
+      series: [{
+        color: 'steelblue',
+        name:  'total savings',
+        data: data
+      }]
+    });
+
+    var x_axis = new Rickshaw.Graph.Axis.X( { 
+      graph: graph,
+      element: document.getElementById('x_axis')
+    });
+
+    var y_axis = new Rickshaw.Graph.Axis.Y( {
+      graph: graph,
+      orientation: 'left',
+      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+      element: document.getElementById('y_axis'),
+    });
+
+    graph.render();
+
+    futureValueString = "$" + futureValue.formatMoney(2);
+    $('#total').val(futureValueString);
+
+    return futureValue;
+  }
+
+  function createBoth(numOfPeriods) {
+    var dataA = [];
+    var dataB = [];
+
+    var interest = $('#interest').val()/100/12;
+    var pmt = $('#pmt').val()
+
+    for(var index = 0; index <= numOfPeriods; index++) {
+      //Instead of passing in numOfPeriods, index is passed into runFvNoInterest)
+      futureValueA = runFvNoInterest(index, pmt);
+      dataA.push({ x: index, y: futureValueA });
+
+      futureValueB = runFVOrdinaryAnnuity(index, pmt, interest);
+      dataB.push({ x: index, y: futureValueB });
+    }
+
+    console.log(dataA);
+    console.log(dataB);
+    // Create a new Rickshaw Graph
+    var graph = new Rickshaw.Graph( {
+      element: document.querySelector("#chart"), 
+      renderer: 'area',
+      stroke: true,
+      series: [ {
+        data: dataA,
+        color: 'steelblue',
+      }, {    
+        data: dataB,
+        color: 'lightblue',
+      } ]    
+    });
+
+    var x_axis = new Rickshaw.Graph.Axis.X( { 
+      graph: graph,
+      element: document.getElementById('x_axis')
+    });
+
+    var y_axis = new Rickshaw.Graph.Axis.Y( {
+      graph: graph,
+      orientation: 'left',
+      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+      element: document.getElementById('y_axis'),
+    });
+
+    graph.render();
+
+    // We look for the total savings of just the Future Value of B
+    futureValueString = "$" + futureValueB.formatMoney(2);
+    $('#total').val(futureValueString);
+
+    return futureValueB;
   }
 
   function testFVforMillionaire(futureValue) {
